389abd55c8ee4c52684f64ab70f41981f4887511
Christopher Snyder <christopher.snyder@omnicell.com>
2020-04-11 00:09:47 -0400

    Finished with user and shop
    This is still using the mock on Amazon Gateway API.

--- WebAPITests/AwsWebApiStoreIntegrationTests.py
+++ WebAPITests/AwsWebApiStoreIntegrationTests.py
@@ -13,15 +13,13 @@ class AwsWebApiFunctionalTests(unittest.TestCase):
         results = r.json()
         self.assertTrue(results[0]['name'] == 'Phantom of the Attic')
         self.assertTrue(results[0]['primaryEmail'] == 'test@pota.com')
-        self.assertTrue(results[0]['uuid'] == '9b70ba73-165c-44b4-a0a0-3acffaab19b7')
-        self.assertTrue(results[0]['_links']['self'] == '/Test/shops/9b70ba73-165c-44b4-a0a0-3acffaab19b7')
+        self.assertTrue(results[0]['_links'][0]['self'] == '/Test/shops/9b70ba73-165c-44b4-a0a0-3acffaab19b7')
         self.assertTrue(results[1]['name'] == 'Drawbridge Games')
         self.assertTrue(results[1]['primaryEmail'] == 'test@drawbridgegames.com')
-        self.assertTrue(results[1]['uuid'] == '726c34d6-3e69-4788-a937-97e416c0626c')
-        self.assertTrue(results[1]['_links']['self'] == '/Test/shops/726c34d6-3e69-4788-a937-97e416c0626c')
+        self.assertTrue(results[1]['_links'][0]['self'] == '/Test/shops/726c34d6-3e69-4788-a937-97e416c0626c')
 
     def test_registering_valid_shop_returns_code_201_and_sets_location_header(self):
-        test_shop = {'name':'Drawbridge Games', 'primaryEmail':'test@drawbridgegames.com', 'uuid':'00000000-0000-0000-0000-000000000000'}
+        test_shop = {'name':'Drawbridge Games', 'primaryEmail':'test@drawbridgegames.com'}
         r = requests.post(url = BASE_URL + '/shops', json = test_shop)
         self.assertTrue(r.status_code == 201)
         self.assertTrue(r.headers['Location'] == '/shops/726c34d6-3e69-4788-a937-97e416c0626c')
@@ -34,27 +32,27 @@ class AwsWebApiFunctionalTests(unittest.TestCase):
     # /Shops/Shop
     def test_getting_a_valid_shop_returns_200_and_json_object(self):
         r = requests.get(url = BASE_URL + '/shops/726c34d6-3e69-4788-a937-97e416c0626c')
-        self.assertTrue(r.json()['name'] == 'Drawbridge Games')
-        self.assertTrue(r.json()['primaryEmail'] == 'test@drawbridgegames.com')
-        self.assertTrue(r.json()['uuid'] == '726c34d6-3e69-4788-a937-97e416c0626c')
-        self.assertTrue(r.json()['_links']['self'] == '/Test/shops/726c34d6-3e69-4788-a937-97e416c0626c')
+        testObject = r.json()
+        self.assertTrue(testObject['name'] == 'Drawbridge Games')
+        self.assertTrue(testObject['primaryEmail'] == 'test@drawbridgegames.com')
+        self.assertTrue(testObject['_links'][0]['self'] == '/Test/shops/726c34d6-3e69-4788-a937-97e416c0626c')
         self.assertTrue(r.status_code == 200)
     
     def test_getting_an_invalid_shop_returns_nothing(self):
         r = requests.get(url = BASE_URL + '/shops/00000000-0000-0000-0000-000000000000')
         self.assertTrue(len(r.text) == 0)
+        self.assertTrue(r.status_code == 200)
 
     def test_valid_update_an_existing_shop_returns_code_204(self):
-        test_shop = {'name':'Drawbridge Games', 'primaryEmail':'test2@drawbridgegames.com', 'uuid':'726c34d6-3e69-4788-a937-97e416c0626c'}
+        test_shop = {'name':'Drawbridge Games', 'primaryEmail':'test2@drawbridgegames.com'}
         r = requests.put(url = BASE_URL + '/shops/726c34d6-3e69-4788-a937-97e416c0626c', json = test_shop)
         self.assertTrue(r.status_code == 204)
     
-    def test_invalid_update_an_existing_shop_returns_code_204(self):
+    def test_update_an_existing_shop_with_invalid_data_returns_code_400(self):
         test_shop = {'name':'D', 'primaryEmail':'test2@drawbridgegames.com', 'shopId':'726c34d6-3e69-4788-a937-97e416c0626c'}
         r = requests.put(url = BASE_URL + '/shops/726c34d6-3e69-4788-a937-97e416c0626c', json = test_shop)
-        self.assertTrue(r.status_code == 404)
-
-
+        self.assertTrue(r.status_code == 400)
+    
 if (__name__ == '__main__'):
     if len(sys.argv) > 1:
         BASE_URL = sys.argv.pop()
--- WebAPITests/AwsWebApiUserIntegrationTests.py
+++ WebAPITests/AwsWebApiUserIntegrationTests.py
@@ -13,21 +13,24 @@ class AwsWebApiFunctionalTests(unittest.TestCase):
         results = r.json()
         self.assertTrue(results[0]['name'] == 'Chris Snyder')
         self.assertTrue(results[0]['email'] == 'basilard99@gmail.com')
-        self.assertTrue(results[0]['userId'] == '7b99059f-62b1-4108-8a04-f7d906a8160d')
-        self.assertTrue(results[0]['_links']['self'] == '/users/7b99059f-62b1-4108-8a04-f7d906a8160d')
+        self.assertTrue(results[0]['userId'] == 'basilard99')
+        self.assertTrue(results[0]['linkedShopIds'][0] == '9b70ba73-165c-44b4-a0a0-3acffaab19b7')
+        self.assertTrue(results[0]['linkedShopIds'][1] == '726c34d6-3e69-4788-a937-97e416c0626c')
+        self.assertTrue(results[0]['_links'][0]['self'] == '/users/7b99059f-62b1-4108-8a04-f7d906a8160d')
         self.assertTrue(results[1]['name'] == 'Jesse Busch')
         self.assertTrue(results[1]['email'] == 'jesse@nodomain.com')
-        self.assertTrue(results[1]['userId'] == 'cbd22a8f-31d6-42f7-81d9-cfff3e604b14')
-        self.assertTrue(results[1]['_links']['self'] == '/users/cbd22a8f-31d6-42f7-81d9-cfff3e604b14')
+        self.assertTrue(results[1]['userId'] == 'jbusch')
+        self.assertTrue(results[1]['linkedShopIds'][0] == '9b70ba73-165c-44b4-a0a0-3acffaab19b7')
+        self.assertTrue(results[1]['_links'][0]['self'] == '/users/cbd22a8f-31d6-42f7-81d9-cfff3e604b14')
 
     def test_registering_valid_user_returns_code_201_and_sets_location_header(self):
-        test_user = {'name':'Chris Snyder', 'email':'basilard99@gmail.com'}
+        test_user = {'name':'Chris Snyder', 'email':'basilard99@gmail.com', 'userId':'basilard99', 'linkedShopIds':[]}
         r = requests.post(url = BASE_URL + '/users', json = test_user)
-        self.assertTrue(r.status_code == 201)
         self.assertTrue(r.headers['Location'] == '/user/7b99059f-62b1-4108-8a04-f7d906a8160d')
+        self.assertTrue(r.status_code == 201)
 
     def test_registering_invalid_user_returns_400(self):
-        test_user = {'name':'D', 'email':'test'}
+        test_user = {'name':'D', 'email':'test', 'userId':'basilard99'}
         r = requests.post(url = BASE_URL + '/users', json = test_user)
         self.assertTrue(r.status_code == 400)
 
@@ -36,6 +39,10 @@ class AwsWebApiFunctionalTests(unittest.TestCase):
         r = requests.get(url = BASE_URL + '/users/7b99059f-62b1-4108-8a04-f7d906a8160d')
         self.assertTrue(r.json()['name'] == 'Chris Snyder')
         self.assertTrue(r.json()['email'] == 'basilard99@gmail.com')
+        self.assertTrue(r.json()['userId'] == 'basilard99')
+        self.assertTrue(r.json()['linkedShopIds'][0] == '9b70ba73-165c-44b4-a0a0-3acffaab19b7')
+        self.assertTrue(r.json()['linkedShopIds'][1] == '726c34d6-3e69-4788-a937-97e416c0626c')
+        self.assertTrue(r.json()['_links'][0]['self'] == '/users/7b99059f-62b1-4108-8a04-f7d906a8160d')
         self.assertTrue(r.status_code == 200)
     
     def test_getting_a_invalid_user_returns_nothing(self):
@@ -43,7 +50,7 @@ class AwsWebApiFunctionalTests(unittest.TestCase):
         self.assertTrue(len(r.text) == 0)
 
     def test_valid_update_an_existing_user_returns_code_204(self):
-        test_user = {'name':'Chris Snyder', 'email':'basilard99+test@gmail.com', 'userId':'7b99059f-62b1-4108-8a04-f7d906a8160d'}
+        test_user = {'name':'Chris Snyder', 'email':'basilard99+test@gmail.com', 'userId':'basilard99', 'linkedShopIds':[]}
         r = requests.put(url = BASE_URL + '/users/7b99059f-62b1-4108-8a04-f7d906a8160d', json = test_user)
         self.assertTrue(r.status_code == 204)
     
